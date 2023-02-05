898f651399ef12cc2951ee5b01d08be3bef26e55
devloper-alamin <dev.alaminsarker@gamil.com>
2023-02-05 17:58:10 +0600

    added if-else conditional

--- class1.php
+++ class1.php
@@ -49,7 +49,7 @@ $rahim = 180;
 // }else{
 //     echo 'This person is not a tenager';
 // }
-
+/*
 $year = 2000;
 if($year % 4==0 && $year % 100==0 && $year % 400==0){
     echo "{$year} is a leap year";
@@ -67,4 +67,71 @@ if($year%4==0 && ($year%100!=0 || $year%400==0)){
     echo "{$year} is a leap year";
 }else{
     echo "{$year} is not leap year";
+}
+*/
+/*
+$condition1 = true;
+$condition2 = true;
+$condition3 = true;
+
+if($condition1 && $condition2 && $condition3){
+    echo "Hello";
+}elseif($condition1 && $condition2){
+    echo "No 2";
+}elseif($condition1){
+    echo "No1";
+}else{
+    echo "No 3";
+}
+*/
+
+/*
+$age = 13;
+if($age>=13 && $age<=19){
+    echo "You are Tenager";
+}else{
+    echo "You are not Tenager";
+}
+echo PHP_EOL;
+echo ($age>=13 && $age<=19)?("You are Tenager"):("You are not Tenager");
+*/
+
+// Leapyear program 
+// Chllange1: daviation = 4
+// Chllange1: daviation = 100
+// Chllange1: daviation = 400
+/*
+$year = 2000;
+
+if($year%4==0 && $year%100==0 && $year%400==0){
+    echo "This is leap year";
+}elseif($year%4==0 && $year%100==0){
+    echo "This is leap year";
+}elseif($year%4==0){
+    echo "This is leap year";
+}
+else{
+    echo "This is not leap year";
+}
+*/
+
+$marks = 110;
+if($marks<33){
+    echo 'Fail!';
+}elseif($marks>=34 && $marks<39){
+    echo "D Grade";
+}elseif($marks>=40 && $marks<49){
+    echo "C Grade";
+}elseif($marks>=50 && $marks<=59){
+    echo "B Grade";
+}elseif($marks>=60 && $marks<=69){
+    echo "A- Grade";
+}elseif($marks>=70 && $marks<=79){
+    echo "A Grade";
+}elseif($marks>=80 && $marks<=100){
+    echo "A+ Grade";
+}elseif($marks>100){
+    echo "Invalid Resutlt";
+}else{
+    "No Result found!";
 }
