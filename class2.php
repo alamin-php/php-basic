9504483bb28da79cfdc5f3425adeadb79f07c867
devloper-alamin <dev.alaminsarker@gamil.com>
2023-02-07 17:56:41 +0600

    added multi stepping for-loop

--- class2.php
+++ class2.php
@@ -1,4 +1,5 @@
 <?php 
+/*
 // 1. different type of loop 
 for($i=1; $i<=10; $i++){
     if($i%2==0){
@@ -28,3 +29,23 @@ do{
     $i++;
     echo $i.PHP_EOL;
 }while($i<10);
+*/
+// 2. Multi stepping for loops 
+/*
+for($i=10; $i>0; $i--){
+    echo $i.":".(11-$i);
+    echo PHP_EOL;
+}
+echo PHP_EOL;
+for($i=10, $j=1; $i>0; $i--, $j++){
+    echo $i.':'.$j;
+    echo PHP_EOL;
+}
+*/
+// 3. More then Multi stepping for loops
+for($i=1; $i<=100; $i++){
+    // echo ($i%7==0)?$i."\n":'';
+    echo ($i%11==0)?$i."\n":'';
+    // echo $i.PHP_EOL;
+    // echo PHP_EOL;
+}
